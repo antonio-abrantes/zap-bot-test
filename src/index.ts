a9e7befb2110083403338e85ba77b079f645b7f7
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2023-10-01 14:06:55 -0300

    ✨ feat: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { Message, Whatsapp, create } from "venom-bot";
+
+create({
+  session: "zap-bot",
+  disableWelcome: true,
+})
+  .then(async (client: Whatsapp) => await start(client))
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function start(client: Whatsapp) {
+  client.onMessage(async (message: Message) => {
+    if (!message.body || message.isGroupMsg) return;
+
+    let response = "";
+
+    response = `Olá! Seu texto foi "${message.content}"`;
+
+    console.log(message.body);
+    console.log(message.from);
+
+    await client.sendText(message.from, response);
+  });
+}
